0319ba910550dc5b1a024d76bd87fd27c0f4681a
LAPTOP-M0F2IN9G\this PC <nguyenthanhlich112002@gmail.com>
2024-02-29 14:22:17 +0700

    commit 29/2

--- frontend/src/component/Slider/slider.jsx
+++ frontend/src/component/Slider/slider.jsx
@@ -1,35 +1,32 @@
+import React from "react";
+import Slider from "react-slick";
+import "./Slider.css"
 
-import React from 'react'
-import Slider from 'react-slick';
-import './Slider.css'
-import { WrapperSliderStyle } from './style';
-
-const SliderComponent = ({ arrImages }) => {
-
-    const settings = {
-        dots: true,
+export default function SimpleSlider() {
+    var settings = {
+        appendDots: true,
         infinite: true,
-        speed: 1000,
+        speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
-        className: "center",
-        centerMode: true,
-        centerPadding: "100px",
+        autoplaySpeed: 1000,
+        accessibility: true
     };
     return (
-        <div className="slider-container" style={{ marginTop: '10px' }}>
-            <WrapperSliderStyle {...settings} >
-                {arrImages.map((image) => {
-                    return (
-                        <img key={image} src={image} alt="slider" preview={false} width='100%' height='400px' style={{ padding: '0 60px' }} />
-                    )
-                })}
-            </WrapperSliderStyle>
-        </div>
-
-    )
+        <Slider {...settings}>
+            <div class="Slider_img">
+                <img src="https://thepizzacompany.vn/images/thumbs/000/0003966_BANNER%20SLIDER%201200X480-100.jpeg" alt="" />
+            </div>
+            <div class="Slider_img">
+                <img src="https://thepizzacompany.vn/images/thumbs/000/0003966_BANNER%20SLIDER%201200X480-100.jpeg" alt="" />
+            </div>
+            <div class="Slider_img">
+                <img src="https://thepizzacompany.vn/images/thumbs/000/0003966_BANNER%20SLIDER%201200X480-100.jpeg" alt="" />
+            </div>
+            <div class="Slider_img">
+                <img src="https://thepizzacompany.vn/images/thumbs/000/0003966_BANNER%20SLIDER%201200X480-100.jpeg" alt="" />
+            </div>
+        </Slider>
+    );
 }
-
-export default SliderComponent
